7120e12c360e3a2cda453da8be57e45c42cd46bf
Roberto Bastos Souza <103335050+RobertoBSZ@users.noreply.github.com>
2022-11-06 13:21:15 -0300

    Add files via upload

--- hr.sql
+++ hr.sql
@@ -68,12 +68,39 @@ BEGIN
     SELECT permissao INTO v_permissao FROM usuario WHERE upper(username) = upper(user);
 
     IF v_permissao = 'C' AND :old.finalizado = 'S' then
-        raise_application_error(-20000,'Não é possivel alterar os dados de um aluno Finalizado');
+        raise_application_error(-20000,'Não é possivel modificar os dados de um aluno Finalizado');
     ELSIF v_permissao = 'A' AND :old.finalizado = 'S' AND DELETING then 
-        raise_application_error(-20000,'Não é possivel alterar os dados de um aluno Finalizado');
+        raise_application_error(-20000,'Não é possivel deletar os dados de um aluno Finalizado');
     END IF;
 
 END;
+
+
+CREATE USER "ADMIN" IDENTIFIED BY "123"  
+DEFAULT TABLESPACE "USERS"
+TEMPORARY TABLESPACE "TEMP";
+
+GRANT "DBA" TO "ADMIN" ;
+GRANT "CONNECT" TO "ADMIN" ;
+GRANT "RESOURCE" TO "ADMIN" ;
+
+GRANT CREATE ROLE TO "ADMIN" ;
+GRANT CREATE TRIGGER TO "ADMIN" ;
+GRANT ALTER SESSION TO "ADMIN" ;
+GRANT CREATE VIEW TO "ADMIN" ;
+GRANT CREATE SESSION TO "ADMIN" ;
+GRANT CREATE RULE TO "ADMIN" ;
+GRANT CREATE TABLE TO "ADMIN" ;
+GRANT CREATE TYPE TO "ADMIN" ;
+GRANT CREATE TABLESPACE TO "ADMIN" ;
+GRANT ALTER USER TO "ADMIN" ;
+GRANT CREATE SEQUENCE TO "ADMIN" ;
+GRANT CREATE USER TO "ADMIN" ;
+GRANT ALTER TABLESPACE TO "ADMIN" ;
+GRANT DROP USER TO "ADMIN" ;
+GRANT ALTER SYSTEM TO "ADMIN" ;
+GRANT DROP TABLESPACE TO "ADMIN" ;
+
 
 insert into alunos (nome) values ('Roberto');
 
@@ -81,15 +108,10 @@ select * from alunos;
 
 update alunos set finalizado = 'S' where cod=1;
 
+CONNECT ADMIN/123@localhost:1521/xepdb1
+
 delete alunos where cod=1;
 
 
 
-
-
-
-
-
-
 commit;
-
